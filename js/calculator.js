0d1c36f703439df90b71b71b10d5327d4fa324e1
Bán Marcell <afga93+cc@gmail.com>
2017-06-05 17:08:52 +0200

    basic working version
    all required features work
    carrying works
    can change sign

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,89 @@
+var inputRow = $('#input-row');
+var prevText = $('#prev-input');
+
+var dotPressed = false;
+var showingResult = false;
+var operator;
+var firstValue;
+var secondValue;
+
 function calculator() {
-    
+    $('button').click(buttonHandler);
+}
+
+function buttonHandler() {
+    var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    var operators = ['+', '-', '*', '/', '%'];
+    var oldInputValue = inputRow.val();
+    var oldPrevText = prevText.text();
+    var buttonText = $(this).text();
+    if (numbers.indexOf(buttonText) >= 0) {
+        if (showingResult) {
+            oldInputValue = '';
+            showingResult = false;
+        }
+        inputRow.val(oldInputValue + buttonText);
+    } else if (buttonText === '.' && !dotPressed) {
+        if (showingResult) {
+            oldInputValue = '';
+            showingResult = false;
+        }
+        dotPressed = true;
+        inputRow.val(oldInputValue + '.');
+    } else if (operators.indexOf(buttonText) >= 0) {
+        showingResult = false;
+        operator = buttonText;
+        firstValue = Number(oldInputValue);
+        dotPressed = false;
+        inputRow.val('');
+        prevText.text(firstValue + ' ' + operator);
+    } else if (buttonText === '+/-') {
+        if (oldInputValue.startsWith('-')) {
+            inputRow.val(oldInputValue.replace('-', ''));
+        } else {
+            inputRow.val('-' + oldInputValue);
+        }
+    } else if (buttonText === '=') {
+        if (!showingResult) {
+            secondValue = Number(oldInputValue);
+        }
+        var result = calculateResult();
+        inputRow.val(result);
+        prevText.text(firstValue + ' ' + operator + ' ' + secondValue + ' =');
+        firstValue = result;
+        showingResult = true;
+        dotPressed = false;
+    } else if (buttonText === 'CE') {
+        showingResult = false;
+        dotPressed = false;
+        firstValue = undefined;
+        secondValue = undefined;
+        operator = undefined;
+        inputRow.val('');
+        prevText.text('');
+    }
+}
+
+function calculateResult() {
+    var result;
+    switch (operator) {
+        case '+':
+            result = firstValue + secondValue;
+            break;
+        case '-':
+            result = firstValue - secondValue;
+            break;
+        case '*':
+            result = firstValue * secondValue;
+            break;
+        case '/':
+            result = firstValue / secondValue;
+            break;
+        case '%':
+            result = firstValue % secondValue;
+            break;
+    }
+    return result;
 }
 
 $(document).ready(calculator);
